6445fa9e8e0c2ae06cada19fa8d84385a56d8f65
Ashish <ashishsaud498@gmail.com>
2025-11-06 01:02:38 +0545

    Set up Express server with MongoDB connection and CORS configuration

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
 
 const app = express();
+dotenv.config({ path: './.env' });
+app.use(cors({
+
+    origin: [process.env.FRONTEND_URL],
+    credentials: true, //to send cookies, authorization headers
+    methods: ['POST'],
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB()
+
 
 export default app;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT}`);
-    
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`server running on port ${process.env.PORT || 5000}`);
+
 })

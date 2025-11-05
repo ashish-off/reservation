bbfde516de7e0bc7e1015b9734a66c3622431c1e
Ashish <ashishsaud498@gmail.com>
2025-11-06 02:48:47 +0545

    add error middleware

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors'
+import { errorMiddleware } from "./error/error.js";
 
 const app = express();
 dotenv.config({ path: './.env' });
 app.use(cors({
-
     origin: [process.env.FRONTEND_URL],
     credentials: true, //to send cookies, authorization headers
     methods: ['POST'],
-}))
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB()
+connectDB();
 
+app.use(errorMiddleware);
 
 export default app;

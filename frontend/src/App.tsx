e833900c7d7be31e25cb4b2deeb7383578dcbcc1
Ashish <ashishsaud498@gmail.com>
2025-12-15 14:22:11 +0545

    Added routing and created Home, Success, and NotFound pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import Success from "./pages/Success";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold text-red-300'>this is ashish</h1>
+    <div className="m-0 p-0 box-border overflow-x-hidden text-black">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

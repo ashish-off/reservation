e7c438df082221e3cf8322729d0265c28e7d091e
Ashish Saud <ashishsaud498@gmail.com>
2026-04-29 23:59:46 +0545

    feat : reservation route fixed

--- backend/src/routes/reservations.route.js
+++ backend/src/routes/reservations.route.js
@@ -1,1 +1,11 @@
 import express from 'express';
+import {
+    getAllReservations,
+} from '../controllers/reservation.controller.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+router.get('/', authMiddleware, getAllReservations);
+
+export default router;
